b693f3ab66eeb127adfca697dc649373db892a50
Pablo Henrique <pablo_hen@hotmail.com>
2021-04-29 13:45:02 -0300

    add seo optimizations

---
+++ pages/_document.js
@@ -0,0 +1,26 @@
+import Document, { Html, Head, Main, NextScript } from 'next/document';
+
+class MyDocument extends Document {
+  static async getInitialProps(ctx) {
+    const initialProps = await Document.getInitialProps(ctx);
+    return { ...initialProps };
+  }
+
+  render() {
+    return (
+      <Html lang="pt-BR">
+        <Head>
+          <title>Hulu 2.0</title>
+          <meta name="description" content="Hulu clone made with Next.js" />
+          <link rel="icon" href="/favicon.ico" />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
